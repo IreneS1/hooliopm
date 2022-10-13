fc0419faf669b9c4add015ccd1a803e7b26a616e
Irene Sanchez <irene.iscc777@gmail.com>
2022-10-13 13:08:45 -0700

    centerd home page and down arrow

--- components/TopHome.js
+++ components/TopHome.js
@@ -1,30 +1,53 @@
 import React from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
-import HomeImage from '../public/images/hooliopmHome.jpg'
 import styles from '../styles/TopHome.module.css'
+import { Text } from "@nextui-org/react";
+import arrowDown from '../public/images/arrowDown.png'
 
 function TopHome() {
-    const [loading, isLoading] = React.useState('true');
 
     return (
         <div className={styles.container}>
             <div className={styles.landingImage}>
-                {/* <Image
-                className={styles.landingImage}
-                src={HomeImage}
-                alt='Home page image'
-                priority
-                placeholder={!isLoading ? 'blur' : 'empty'}
-                objectFit='cover'
-                objectPosition='center'
-            /> */}
-                <h1>Welcome</h1>
-                <h2 >
-                    <Link href="/Contact">Contact page!</Link>
-                </h2>
+                <Text
+                    h1
+                    size={60}
+                    css={{
+                        textGradient: "45deg, $green600 -20%, $blue600 50%",
+                        textAlign: "center",
+                        padding: "10px",
+                        marginTop: "100px",
+                    }}
+                    weight="light"
+                >
+                    Welcome
+                </Text>
+                <Text
+                    h4
+                    size={60}
+                    css={{
+                        textGradient: "45deg, $blue600 -20%, $green600 50%",
+                        textAlign: "center",
+                    }}
+                    weight="hairline"
+                >
+                    <Link href="/Contact">Contact Me</Link>
+                </Text>
+
+
                 <h3>More text</h3>
                 <br />
+                <div className={styles.arrowDown}>
+                    <Image
+                        priority
+                        src={arrowDown}
+                        alt='down-arrow'
+                        width={90}
+                        height={90}
+
+                    />
+                </div>
             </div>
         </div>
     )
